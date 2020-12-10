f2cfa18c4ea1e1894a1c4be1399937b67ccc90c0
Robert Bilsland <RBilsland@users.noreply.github.com>
2020-12-10 17:22:25 +0000

    Day 10 Complete

---
+++ Day10a/Day10a/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace Day10a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"..\..\..\Input.txt");
+            List<int> adapters = Array.ConvertAll(input, int.Parse).ToList();
+
+            adapters.Add(0);
+            adapters.Add(adapters.Max() + 3);
+
+            int[] sortedAdapters = adapters.OrderBy(adapter => adapter).ToArray();
+
+            int runningJoltDifferences = 0;
+
+            Dictionary<int, int> joltDifferences = new Dictionary<int, int>();
+
+            for(int position = 1; position < sortedAdapters.Length; position ++)
+            {
+                runningJoltDifferences += sortedAdapters[position] - sortedAdapters[position - 1];
+
+                int joltDifference = sortedAdapters[position] - sortedAdapters[position - 1];
+
+                if (!joltDifferences.ContainsKey(joltDifference))
+                {
+                    joltDifferences.Add(joltDifference, 0);
+                }
+
+                joltDifferences[joltDifference]++;
+            }
+
+            Console.WriteLine("AoC 2020 - Day 10a");
+            Console.WriteLine($"Running Jolt Differences: {runningJoltDifferences}");
+
+            foreach(KeyValuePair<int, int> joltDifference in joltDifferences)
+            {
+                Console.WriteLine($"Jolt Difference: {joltDifference.Key} Count: {joltDifference.Value}");
+            }
+
+            Console.WriteLine($"Answer: {joltDifferences[1] * joltDifferences[3]}");
+        }
+    }
+}
---
+++ Day10b/Day10b/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"..\..\..\Input.txt");
+            List<int> adapters = Array.ConvertAll(input, int.Parse).ToList();
+
+            adapters.Add(0);
+            adapters.Add(adapters.Max() + 3);
+
+            Dictionary<int, long> paths = adapters.ToDictionary(adapter => adapter, adapter => 0L);
+
+            paths[adapters.Min()] = 1;
+
+            foreach(int fromAdapter in adapters.OrderBy(adapter => adapter))
+            {
+                foreach(int toAdapter in adapters.Where(adapter => adapter > fromAdapter && adapter <= fromAdapter + 3))
+                {
+                    paths[toAdapter] += paths[fromAdapter];
+                }
+            }
+
+            Console.WriteLine("AoC 2020 - Day 10b");
+            Console.WriteLine($"Permutations: {paths.Where(path => path.Key == adapters.Max()).FirstOrDefault().Value}");
+        }
+    }
+}
