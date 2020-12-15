1995d0048ead6934379477817997012591b4647b
Robert Bilsland <RBilsland@users.noreply.github.com>
2020-12-15 17:30:28 +0000

    Day 15 Complete

---
+++ Day15a/Day15a/NumberSpoken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day15a
+{
+    public class NumberSpoken
+    {
+        public int Difference { get; set; } = 0;
+        public int LastSpoken { get; set; } = 0;
+        public int Occurances { get; set; } = 0;
+    }
+}
---
+++ Day15b/Day15b/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Day15b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"..\..\..\Input.txt");
+
+            int[] numbers = input[0]
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Where(number => int.TryParse(number, out _))
+                .Select(int.Parse)
+                .ToArray();
+
+            int turn = 1;
+            Dictionary<int, NumberSpoken> numbersSpoken = new Dictionary<int, NumberSpoken>();
+            int lastSpokenNumber = 0;
+
+            foreach(int number in numbers)
+            {
+                lastSpokenNumber = number;
+
+                numbersSpoken = numbersSpoken.UpdateNumberSpoken(number, turn);
+
+                turn++;
+            }
+
+            while (numbersSpoken.ContainsKey(lastSpokenNumber) && turn <= 30000000)
+            {
+                if (numbersSpoken[lastSpokenNumber].Occurances == 1)
+                {
+                    lastSpokenNumber = 0;
+                }
+                else
+                {
+                    lastSpokenNumber = numbersSpoken[lastSpokenNumber].Difference;
+                }
+
+                numbersSpoken = numbersSpoken.UpdateNumberSpoken(lastSpokenNumber, turn);
+
+                turn++;
+            }
+
+            Console.WriteLine("AoC 2020 - Day 15b");
+            Console.WriteLine($"Last Spoken Number: {lastSpokenNumber}");
+        }
+    }
+}
