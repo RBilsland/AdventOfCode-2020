384df869f69220c62f263a29fba82717ec0e3859
Robert Bilsland <RBilsland@users.noreply.github.com>
2020-12-18 21:35:17 +0000

    Day 18 Complete

---
+++ Day18a/Day18a/Formula.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Day18a
+{
+    public static class Formula
+    {
+        public static string Process(string input)
+        {
+            int currentPos = 0;
+
+            while (currentPos < input.Length)
+            {
+                if (input[currentPos] == '(')
+                {
+                    currentPos++;
+
+                    int startPos = currentPos;
+                    int openBracketCount = 1;
+
+                    while (openBracketCount > 0)
+                    {
+                        switch (input[currentPos])
+                        {
+                            case '(':
+                                openBracketCount++;
+                                break;
+                            case ')':
+                                openBracketCount--;
+                                break;
+                        }
+                        currentPos++;
+                    }
+                    int originalLength = input.Length;
+
+                    input = $"{input[..(startPos - 1)]}{Process(input[startPos..(currentPos - 1)])}{input[(currentPos)..]}";
+
+                    currentPos -= (originalLength - input.Length);
+                }
+                else
+                {
+                    currentPos++;
+                }
+            }
+
+            return Calculate(input);
+        }
+
+        public static string Calculate(string input)
+        {
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            long accumulator = 0;
+            string lastOperator = string.Empty;
+
+            foreach(string part in parts)
+            {
+                long number = 0;
+
+                if (long.TryParse(part, out number))
+                {
+                    if (lastOperator == string.Empty)
+                    {
+                        accumulator = number;
+                    }
+                    else
+                    {
+                        switch (lastOperator)
+                        {
+                            case "+":
+                                accumulator += number;
+                                break;
+                            case "*":
+                                accumulator *= number;
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    lastOperator = part;
+                }
+            }
+
+            return accumulator.ToString();
+        }
+    }
+}
---
+++ Day18b/Day18b/Formula.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day18b
+{
+    public static class Formula
+    {
+        public static string Process(string input)
+        {
+            int currentPos = 0;
+
+            while (currentPos < input.Length)
+            {
+                if (input[currentPos] == '(')
+                {
+                    currentPos++;
+
+                    int startPos = currentPos;
+                    int openBracketCount = 1;
+
+                    while (openBracketCount > 0)
+                    {
+                        switch (input[currentPos])
+                        {
+                            case '(':
+                                openBracketCount++;
+                                break;
+                            case ')':
+                                openBracketCount--;
+                                break;
+                        }
+                        currentPos++;
+                    }
+                    int originalLength = input.Length;
+
+                    input = $"{input[..(startPos - 1)]}{Process(input[startPos..(currentPos - 1)])}{input[(currentPos)..]}";
+
+                    currentPos -= (originalLength - input.Length);
+                }
+                else
+                {
+                    currentPos++;
+                }
+            }
+
+            return Calculate(input);
+        }
+
+        public static string Calculate(string input)
+        {
+            List<string> parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            int position = 1;
+
+            while (position < parts.Count() - 1)
+            {
+                if (parts[position] == "+")
+                {
+                    parts[position - 1] = (long.Parse(parts[position - 1]) + long.Parse(parts[position + 1])).ToString();
+
+                    parts.RemoveRange(position, 2);
+                }
+                else
+                {
+                    position++;
+                }
+            }
+
+            position = 1;
+
+            while (position < parts.Count() - 1)
+            {
+                if (parts[position] == "*")
+                {
+                    parts[position - 1] = (long.Parse(parts[position - 1]) * long.Parse(parts[position + 1])).ToString();
+
+                    parts.RemoveRange(position, 2);
+                }
+                else
+                {
+                    position++;
+                }
+            }
+
+            return parts[0].ToString();
+        }
+    }
+}
